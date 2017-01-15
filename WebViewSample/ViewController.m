fea0f3e7aefd9ef641b481283a9192a7d4975aa9
aCetylAcid <aka.l.m1ura.setsuna.karimofu@gmail.com>
2017-01-15 19:49:00 +0900

    ウェブページの表示を実装

--- WebViewSample/ViewController.m
+++ WebViewSample/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define INITIAL_URL @"https://amazon.co.jp/"
+
+@interface ViewController () <WKNavigationDelegate>
+
+@property (strong, nonatomic) WKWebView *webView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *reloadButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *stopButton;
 
 @end
 
@@ -16,14 +24,73 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // WKWebView インスタンスのプロパティの変更を監視する
+//    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+//    [self.webView addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
+//    [self.webView addObserver:self forKeyPath:@"loading" options:NSKeyValueObservingOptionNew context:nil];
+//    [self.webView addObserver:self forKeyPath:@"canGoBack" options:NSKeyValueObservingOptionNew context:nil];
+//    [self.webView addObserver:self forKeyPath:@"canGoForward" options:NSKeyValueObservingOptionNew context:nil];
+    
+    // 初回画面表示時にIntialURLで指定した Web ページを読み込む
+    NSURL *url = [NSURL URLWithString:INITIAL_URL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
 }
 
+- (void)loadView
+{
+    [super loadView];
+    
+    // WKWebView インスタンスの生成
+    self.webView = [WKWebView new];
+    
+    // Auto Layout の設定
+    // 画面いっぱいに WKWebView を表示するようにする
+    self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraints:@[
+                                [NSLayoutConstraint constraintWithItem:self.webView
+                                                             attribute:NSLayoutAttributeWidth
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:self.view
+                                                             attribute:NSLayoutAttributeWidth
+                                                            multiplier:1.0
+                                                              constant:0],
+                                [NSLayoutConstraint constraintWithItem:self.webView
+                                                             attribute:NSLayoutAttributeHeight
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:self.view
+                                                             attribute:NSLayoutAttributeHeight
+                                                            multiplier:1.0
+                                                              constant:0]
+                                ]];
+    
+    // デリゲートにこのビューコントローラを設定する
+    self.webView.navigationDelegate = self;
+    
+    // フリップでの戻る・進むを有効にする
+    self.webView.allowsBackForwardNavigationGestures = YES;
+    
+    // WKWebView インスタンスを画面に配置する
+    [self.view insertSubview:self.webView atIndex:0];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)decideBtnTapped:(id)sender {
+    NSString *msg = [NSString stringWithFormat:@"URL: %@", self.webView.URL];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"INFO"
+                                                                 message:msg
+                                                          preferredStyle:UIAlertControllerStyleAlert];
+    [controller addAction:cancelAction];
+    [self presentViewController:controller animated:YES completion:nil];
+}
 
 @end
